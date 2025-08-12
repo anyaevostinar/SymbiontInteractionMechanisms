be8837ff3ffa2db455c4f6354b7fea17464d3721
Anya Vostinar <vostinar@carleton.edu>
2025-08-12 10:18:50 -0500

    setting up bash script

---
+++ Data/8-11-25-HealthMode/run-mutualists.sh
@@ -0,0 +1,32 @@
+#!/bin/bash --login
+#SBATCH -A ecode
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=vostinar@carleton.edu
+
+## Job name settings
+#SBATCH --job-name=health-mut
+#SBATCH -o health-mut%A_%a.out
+
+## Time requirement in hours and minutes. You might need to make this
+## longer, but try to keep it under 4 hours if possible
+#SBATCH --time=0-12
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=500M
+
+## Launch an array of jobs.
+#SBATCH --array=100-130
+
+
+cd /Accounts/vostinar/SymbiontInteractionMechanisms/Data/8-11-25-HealthMode
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+args=" -START_MOI 1 -FILE_NAME _Mutualists -SYMBIONT_TYPE 0 -CPU_TRANSFER_CHANCE 0.65 -VERTICAL_TRANSMISSION 1.0 "
+./symbulation_default $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
---
+++ Data/8-11-25-NutrientMode/simple_repeat.py
@@ -0,0 +1,55 @@
+#a script to run several replicates of several treatments locally
+#You should create a directory for your result files and run this script from within that directory
+# usage: python3 simple_repeat.py <start_seed> <end_seed>
+# Assumes that symbulation executable and SymSettings.cfg already in the folder
+
+import subprocess
+import sys
+
+treatments = [" -START_MOI 0 -FILE_NAME _NoSyms ", #no symbionts
+              " -START_MOI 1 -FILE_NAME _Parasites -SYMBIONT_TYPE 1 -CPU_TRANSFER_CHANCE 0.8 -VERTICAL_TRANSMISSION 0.0",
+              " -START_MOI 1 -FILE_NAME _Mutualists -SYMBIONT_TYPE 0 -CPU_TRANSFER_CHANCE 0.65 -VERTICAL_TRANSMISSION 1.0"]
+
+filenames = ["_NoSyms", "_Parasites", "_Mutualists"]
+
+def cmd(command):
+    '''This wait causes all executions to run in sieries.                          
+    For parralelization, remove .wait() and instead delay the                      
+    R script calls unitl all neccesary data is created.'''
+    return subprocess.Popen(command, shell=True).wait()
+
+def silent_cmd(command):
+    '''This wait causes all executions to run in sieries.                          
+    For parralelization, remove .wait() and instead delay the                      
+    R script calls unitl all neccesary data is created.'''
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).wait()
+
+start_range = 10
+end_range = 20
+
+#collect optional command line arguments
+if(len(sys.argv) > 1):
+    #first argument is the starting range for seeds
+    start_range = int(sys.argv[1])
+    if(len(sys.argv) > 2):
+        #if the user provides a second argument, use it
+        #as the inclusive end of the seed range
+        end_range = int(sys.argv[2]) + 1
+    else:
+        #if the user does not provide a second argument,
+        #set the seed range as just the single seed 
+        #indicated by the first argument
+        end_range = start_range + 1
+
+seeds = range(start_range, end_range)
+
+#Tell the user the inclusive range of seeds
+print("Using seeds", start_range, "through", end_range-1)
+
+for a in seeds:
+    for i in range(len(treatments)):
+        command_str = './symbulation_sgp -SEED '+str(a)+ treatments[i]
+        settings_filename = "Output_"+filenames[i]+".data"
+
+        print(command_str)
+        cmd(command_str+" > "+settings_filename)
