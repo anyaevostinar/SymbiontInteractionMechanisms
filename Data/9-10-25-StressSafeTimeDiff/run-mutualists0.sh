981410382b9c61bf563ebb7080441510da1f4ee7
Camila <mendozac@carleton.edu>
2025-09-11 15:28:28 -0500

    files

---
+++ Data/9-10-25-StressSafeTimeDiff/run-mutualists0.sh
@@ -0,0 +1,39 @@
+#!/bin/bash --login
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=vostinar@carleton.edu
+
+## Job name settings
+#SBATCH --job-name=sm-1k
+#SBATCH -o sm-1k%A_%a.out
+
+## Time requirement in hours and minutes. You might need to make this
+## longer, but try to keep it under 4 hours if possible
+## SBATCH --time=0-12
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=500M
+
+## Launch an array of jobs.
+#SBATCH --array=100-129
+
+#SBATCH --nodes=1
+
+cd /Accounts/mendozac/SymbiontInteractionMechanisms/Data/9-10-25-StressSafeTimeDiff
+mkdir Mutualists0
+cd Mutualists0
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+cp /Accounts/mendozac/SymbiontInteractionMechanisms/Data/9-10-25-StressSafeTimeDiff/SymSettings.cfg .
+cp /Accounts/mendozac/SymbiontInteractionMechanisms/SymbulationEmp/symbulation_sgp .
+
+args=" -START_MOI 1 -FILE_NAME _Mutualists0 -SYMBIONT_TYPE 0 -VERTICAL_TRANSMISSION 1.0 -SAFE_TIME 0"
+./symbulation_sgp $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
+
+## Run with sbatch -p facultynode --nodelist=edmonstone2024,margulis2024,carver,lederberg run-mutualists0.sh
