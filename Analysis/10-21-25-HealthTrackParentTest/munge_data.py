aed5fa5cce7c468f33123a81a8e12d4cad607359
Emma Zhang <iszhangruoyu@gmail.com>
2025-10-21 21:27:34 -0500

    test setting

---
+++ Analysis/10-21-25-HealthTrackParentTest/munge_data.py
@@ -0,0 +1,33 @@
+import os.path
+import gzip
+
+metafolder = '../../Data/10-21-25-HealthTrackParentTest/'
+
+
+folders = ["Parasites-80-25", "Parasites-100-25", "Parasites-150-25"]
+partners = ["Host", "Sym"]
+tasks = ["NOT", "NAND", "AND", "ORN", "OR", "ANDN", "NOR", "XOR", "EQU"]
+reps = range(100,131)
+header = "uid treatment rep update task count partner\n"
+
+outputFileName = "munged_basic.dat"
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for t in folders:
+    for r in reps:
+        fname = metafolder +t+"/"+str(r)+"/" + "Tasks_" + t +"_SEED" + str(r)+ ".data"
+        uid = t + "_" + str(r)      
+
+        curFile = open(fname, 'r')
+        for line in curFile:
+            if (line[0] != "u"):
+                splitline = line.strip().split(',')
+                update = splitline[0]
+                sym_steal_ran = splitline[1] 
+                sym_donate_ran = splitline[2]
+                outstring = "{} {} {} {} {} {}\n".format(uid, t, r, update, sym_steal_ran, sym_donate_ran)
+                outFile.write(outstring)
+        curFile.close()
+        curFile.close()
+outFile.close()
---
+++ Analysis/10-21-25-HealthTrackParentTest/munge_syminstcount.py
@@ -0,0 +1,30 @@
+import os.path
+import gzip
+
+metafolder = '../../Data/10-21-25-HealthTrackParentTest/'
+
+
+folders = ["Parasites-80-25", "Parasites-100-25", "Parasites-150-25"]
+reps = range(100,131)
+header = "uid treatment rep update sym_steal_ran sym_donate_ran"
+
+outputFileName = "munged_syminstcount.dat"
+outFile = open(outputFileName, 'w')
+outFile.write(header+"\n")
+
+for t in folders:
+    for r in reps:
+        fname = metafolder +t+"/"+str(r)+"/" + "SymInstCount_" + t +"_SEED" + str(r)+ ".data"
+        uid = t + "_" + str(r)      
+
+        curFile = open(fname, 'r')
+        for line in curFile:
+            if (line[0] != "u"):
+                splitline = line.strip().split(',')
+                update = splitline[0]
+                sym_steal_ran = splitline[1] 
+                sym_donate_ran = splitline[2]
+                outstring = "{} {} {} {} {} {}\n".format(uid, t, r, update, sym_steal_ran, sym_donate_ran)
+                outFile.write(outstring)
+        curFile.close()
+outFile.close()
