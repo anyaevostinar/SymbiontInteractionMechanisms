45f1248be6bb963c0fe38793efb07dbbaf1072dc
Emma Zhang <iszhangruoyu@gmail.com>
2025-10-21 19:53:12 -0500

    files

---
+++ Analysis/10-15-25-HealthMinCycleTest/munge_data_syminstcount.py
@@ -0,0 +1,29 @@
+import os.path
+import gzip
+
+metafolder = '../../Data/10-15-25-HealthMinCycleTest/'
+
+
+folders = ["Parasites-80-25", "Parasites-100-25", "Parasites-150-25"]
+reps = range(100,131)
+header = "update sym_steal_ran sym_donate_ran"
+
+outputFileName = "munged_syminstcount.dat"
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for t in folders:
+    for r in reps:
+        fname = metafolder +t+"/"+str(r)+"/" + "SymInstCount_" + t +"_SEED" + str(r)+ ".data"
+        uid = t + "_" + str(r)      
+
+        curFile = open(fname, 'r')
+        for line in curFile:
+            if (line[0] != "u"):
+                splitline = line.strip().split(',')
+                update = splitline[0]
+                sym_steal_ran = splitline[1] 
+                sym_donate_ran = splitline[2]
+                outFile.write(line)
+        curFile.close()
+outFile.close()
