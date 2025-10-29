ea06a9d339e2f94e07622f54ca2467b062dd4357
Emma Zhang <iszhangruoyu@gmail.com>
2025-10-28 22:49:49 -0500

    files

---
+++ Data/10-28-25-HealthTrackParentTest/run-parasites-100-25.sh
@@ -0,0 +1,39 @@
+#!/bin/bash --login
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tuckerd@carleton.edu
+
+## Job name settings
+#SBATCH --job-name=str-para
+#SBATCH -o str-para%A_%a.out
+
+## Time requirement in hours and minutes. You might need to make this
+## longer, but try to keep it under 4 hours if possible
+## SBATCH --time=0-24
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=500M
+
+## Launch an array of jobs.
+#SBATCH --array=100-130
+
+#SBATCH --nodes=1
+
+cd /Accounts/zhange2/SymbiontInteractionMechanisms/Data/10-28-25-HealthTrackParentTest
+mkdir Parasites-100-25
+cd Parasites-100-25
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+cp /Accounts/zhange2/SymbiontInteractionMechanisms/Data/10-28-25-HealthTrackParentTest/SymSettings.cfg .
+cp /Accounts/zhange2/SymbiontInteractionMechanisms/SymbulationEmp/symbulation_sgp .
+
+args=" -START_MOI 1 -FILE_NAME _Parasites-100-25 -SYMBIONT_TYPE 1 -VERTICAL_TRANSMISSION 0.0 -HOST_MIN_CYCLES_BEFORE_REPRO 100 -SYM_MIN_CYCLES_BEFORE_REPRO 25"
+./symbulation_sgp $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
+
+## Run with sbatch -p facultynode --nodelist=edmonstone2024,margulis2024,carver,lederberg run-parasites-100-25.sh
---
+++ Data/10-28-25-HealthVerticalTrans/run-parasites-0.0.sh
@@ -0,0 +1,39 @@
+#!/bin/bash --login
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tuckerd@carleton.edu
+
+## Job name settings
+#SBATCH --job-name=str-para
+#SBATCH -o str-para%A_%a.out
+
+## Time requirement in hours and minutes. You might need to make this
+## longer, but try to keep it under 4 hours if possible
+## SBATCH --time=0-24
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=500M
+
+## Launch an array of jobs.
+#SBATCH --array=100-130
+
+#SBATCH --nodes=1
+
+cd /Accounts/zhange2/SymbiontInteractionMechanisms/Data/10-28-25-HealthVerticalTrans
+mkdir Parasites-0.0
+cd Parasites-0.0
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+cp /Accounts/zhange2/SymbiontInteractionMechanisms/Data/10-28-25-HealthVerticalTrans/SymSettings.cfg .
+cp /Accounts/zhange2/SymbiontInteractionMechanisms/SymbulationEmp/symbulation_sgp .
+
+args=" -START_MOI 1 -FILE_NAME _Parasites-0.0 -SYMBIONT_TYPE 1 -VERTICAL_TRANSMISSION 0.0"
+./symbulation_sgp $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
+
+## Run with sbatch -p facultynode --nodelist=edmonstone2024,margulis2024,carver,lederberg run-parasites-0.0.sh
