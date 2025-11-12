40dcd7db67004a4ac2794eb98bea6cac9c385b23
dayatucker <dvtucker04@gmail.com>
2025-11-11 19:46:12 -0600

    new escapees

---
+++ Data/11-11-25-EscapeesNoPrefOust/run-parasites-1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash --login
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tuckerd@carleton.edu
+
+## Job name settings
+#SBATCH --job-name=str-para
+#SBATCH -o str-para%A_%a.out
+
+## Time requirement in hours and minutes. You might need to make this
+## longer, but try to keep it under 4 hours if possible
+## SBATCH --time=0-24
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=500M
+
+## Launch an array of jobs.
+#SBATCH --array=100-130
+
+#SBATCH --nodes=1
+
+cd /Accounts/tuckerd/SymbiontInteractionMechanisms/Data/11-11-25-EscapeesNoPrefOust
+mkdir Parasites-1
+cd Parasites-1
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+cp /Accounts/tuckerd/SymbiontInteractionMechanisms/Data/11-11-25-EscapeesNoPrefOust/SymSettings.cfg .
+cp /Accounts/tuckerd/SymbiontInteractionMechanisms/SymbulationEmp/symbulation_sgp .
+
+args=" -START_MOI 1 -FILE_NAME _Parasites-1 -PARASITE_NUM_OFFSPRING_ON_STRESS_INTERACTION 1"
+./symbulation_sgp $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
+
+## Run with sbatch -p facultynode --nodelist=edmonstone2024,margulis2024,carver,lederberg run-parasites-1.sh
